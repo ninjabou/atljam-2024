37e210b38b3212d104ccced6786227622faab4ba
ninjabou <devroo2000@gmail.com>
2024-12-07 18:59:48 -0500

    hitstop

--- scenes/kickables/goal/goal.gd
+++ scenes/kickables/goal/goal.gd
@@ -9,6 +9,10 @@ func _ready() -> void:
 	sprite.play("unkicked")
 
 func kicked():
+	set_collision_layer_value(3, false)
+	var tween: Tween = create_tween()
+	tween.tween_property(sprite, "modulate:v", 20, 0.1).from(15)
+	tween.tween_property(sprite, "modulate:v", 0.7, 0.1).from(15)
 	sprite.play("kicked")
 	particles.emitting = true
 	SceneManager.goto_scene(next_level)
