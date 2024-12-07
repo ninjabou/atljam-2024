81eae72f11a8a8da30c564b61572b7108e437eaa
ninjabou <devroo2000@gmail.com>
2024-12-06 22:20:06 -0500

    Add state machine

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,12 +15,12 @@ func _physics_process(delta: float) -> void:
 		speed = FLOOR_SPEED
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("action_left", "action_right")
 	if direction:
 		velocity.x = direction * speed
 	else:
