720ef822a65ed56a4f1c97babaf636dc81de3781
ninjabou <devroo2000@gmail.com>
2024-12-07 19:44:50 -0500

    add next never

--- scenes/kickables/enemies/default_enemy/default_enemy.gd
+++ scenes/kickables/enemies/default_enemy/default_enemy.gd
@@ -2,9 +2,14 @@ extends Kickable
 class_name DefaultEnemy
 
 @onready var sprite: AnimatedSprite2D = $Sprite2D
+@onready var particles: GPUParticles2D = $GPUParticles2D
 
 func kicked():
 	set_collision_layer_value(4, false)
 	var tween: Tween = create_tween()
 	tween.tween_property(sprite, "modulate:v", 20, 0.1).from(15)
+	tween.tween_callback(enable_particles)
 	tween.tween_callback(queue_free)
+
+func enable_particles():
+	particles.emitting = true
